0b213aa82a5aeecf5ec3fb49283b5d61157171ca
WallnussJonas <65344424+WallnussJonas@users.noreply.github.com>
2024-10-11 21:29:42 +0200

    removed useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
